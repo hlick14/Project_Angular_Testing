55a7a710d5f017a261000431b4aa027e571bc5af
Jakub Pieczonka <hlick14@gmail.com>
2017-01-31 22:20:53 +0000

    Testing the controller and a service that works of real API.Using httpbackend to listen to http endpoints

--- app/services/users/users.js
+++ app/services/users/users.js
@@ -1,43 +1,43 @@
 (function() {
   'use strict';
 
-  // Creating the module and factory we referenced in the beforeEach blocks in our test file
   angular.module('api.users', [])
   .factory('Users', function() {
     var Users = {};
-
-    // Users.method = function() {};
-     var userList = [
+    var userList = [
       {
         id: '1',
         name: 'Jane',
         role: 'Designer',
         location: 'New York',
-        twitter: 'gijane'
+        twitter: 'gijane',
+        pokemon: { name: 'blastoise' }
       },
       {
         id: '2',
         name: 'Bob',
         role: 'Developer',
         location: 'New York',
-        twitter: 'billybob'
+        twitter: 'billybob',
+        pokemon: { name: 'growlithe' }
       },
       {
         id: '3',
         name: 'Jim',
         role: 'Developer',
         location: 'Chicago',
-        twitter: 'jimbo'
+        twitter: 'jimbo',
+        pokemon: { name: 'hitmonchan' }
       },
       {
         id: '4',
         name: 'Bill',
         role: 'Designer',
         location: 'LA',
-        twitter: 'dabill'
+        twitter: 'dabill',
+        pokemon: { name: 'barney' }
       }
     ];
-
     Users.all = function() {
        // Returning the array of users. Eventually this will be an API call.
       return userList;
